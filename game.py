02285106a532f6d41d092bb164cfd64d30ea08e9
thilompas <thiagodesousacosta@outlook.com>
2020-02-05 23:21:30 -0300

    colocando chamadas dos tipos de jogadores

--- game.py
+++ game.py
@@ -3,12 +3,11 @@ import random
 from player import Jogador
 
 QUANTIDADE_CASAS = 20
-QUANTIDADE_JOGADORES = 4
 
 class Casa():
     def __init__(self):
         self.numero_casa = 0
-        self.valor_compra = 0
+        self.valor_casa = 0
         self.valor_aluguel = 0
 
 class Tabuleiro():
@@ -16,19 +15,20 @@ class Tabuleiro():
         
         self.casas = []
 
-        for x in range(QUANTIDADE_CASAS - 1):
+        for x in range(QUANTIDADE_CASAS):
             casa = Casa()
             casa.numero_casa = x + 1
-            casa.valor_compra = random.randrange(10,200,10)
-            casa.valor_aluguel = casa.valor_compra * 0.1
+            casa.valor_casa = random.randrange(10,200,10)
+            casa.valor_aluguel = casa.valor_casa * 0.1
             self.casas.append(casa)
 
         for jogador in jogadores:
             jogador.dinheiro = 300
         
-        set_trace()
-
 if __name__ == "__main__":
-    jogadores = [Jogador() for x in range(QUANTIDADE_JOGADORES)]
-    Tabuleiro(jogadores)
-    set_trace()
+    jogador1 = Jogador('impulsivo')
+    jogador2 = Jogador('exigente')
+    jogador3 = Jogador('cauteloso')
+    jogador4 = Jogador('aleatorio')
+
+    Tabuleiro([jogador1, jogador2, jogador3, jogador4])
