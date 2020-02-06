26282484019310a9745754d23f5e376c14b673e1
thilompas <thiagodesousacosta@outlook.com>
2020-02-06 03:40:23 -0300

    considerações finais
    Adicionado o arquivo main.py para centralizar a execução

--- game.py
+++ game.py
@@ -1,11 +1,7 @@
-import statistics
-from pdb import set_trace
-from random import randint, randrange, sample
-from player import Jogador
+from random import randrange
 
 QUANTIDADE_CASAS        = 20
 QUANTIDADE_ROUNDS       = 1000
-QUANTIDADE_SIMULACOES   = 300
 DINHEIRO_POR_ROUND      = 100
 PORCENTAGEM_ALUGUEL     = 0.2
 
@@ -65,33 +61,3 @@ class Tabuleiro():
         timeout = 1
         jogador_ganhador = [x.tipo_jogador for x in self.jogadores if x.dinheiro == max([x.dinheiro for x in self.jogadores])][0]
         return timeout, jogador_ganhador, rounds
-
-if __name__ == "__main__":
-    resultado_final = []
-    for x in range(QUANTIDADE_SIMULACOES):
-        jogadores = [Jogador(x) for x in sample(range(1,5), 4)]
-        tabuleiro = Tabuleiro(jogadores)
-        resultado = tabuleiro.jogar()
-        resultado_final.append(resultado)
-
-    partidas_timeout = sum([x[0] for x in resultado_final if x[0] == 1])
-    media_turno = round(sum([x[2] for x in resultado_final])/QUANTIDADE_SIMULACOES)
-
-    resultado_exigente = (len([x[1] for x in resultado_final if x[1] == 'exigente'] )/QUANTIDADE_SIMULACOES) * 100
-    resultado_impulsivo  = (len([x[1] for x in resultado_final if x[1] == 'impulsivo'])/QUANTIDADE_SIMULACOES) * 100
-    resultado_cauteloso = (len([x[1] for x in resultado_final if x[1] == 'cauteloso'])/QUANTIDADE_SIMULACOES) * 100
-    resultado_aleatorio = (len([x[1] for x in resultado_final if x[1] == 'aleatorio'])/QUANTIDADE_SIMULACOES) * 100
-    
-    comportamento_ganhador = statistics.mode([x[1] for x in resultado_final])
-
-    print(f'''
-        Partidas Time Out   = {partidas_timeout}
-        Média de turnos     = {media_turno}
-        
-        Resultado Exigente  = {resultado_exigente}
-        Resultado Impulsivo = {resultado_impulsivo}
-        Resultado Cauteloso = {resultado_cauteloso}
-        Resultado Aleatório = {resultado_aleatorio}
-
-        Comportamento Ganhador = {comportamento_ganhador}
-                ''')
