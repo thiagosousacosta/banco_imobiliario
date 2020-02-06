fd5d4a4c8c7e842ac0142f07e24cc9dac72999d6
thilompas <thiagodesousacosta@outlook.com>
2020-02-05 23:20:52 -0300

    implementando tipos de jogadores

--- player.py
+++ player.py
@@ -1,5 +1,23 @@
-
+from random import randint
 
 class Jogador():
-    def __init__(self):
+    def __init__(self, tipo_jogador):
         self.dinheiro = 0
+        self.tipo_jogador = tipo_jogador
+
+    def pode_comprar(self, valor_aluguel, valor_casa):
+        if self.tipo_jogador == 'impulsivo':
+            if self.dinheiro >= valor_casa:
+                return True
+
+        elif self.tipo_jogador == 'exigente':
+            if valor_aluguel > 50:
+                return True
+
+        elif self.tipo_jogador == 'cauteloso':
+            if self.dinheiro - valor_casa >= 80:
+                return True
+
+        elif self.tipo_jogador == 'aleatorio':
+            if randint(1,10) > 5:
+                return True
